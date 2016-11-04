dbb04ad089a850cc0ead85451092be268449fa33
stenoritama <shr.nrht.0623@gmail.com>
2016-11-04 17:22:03 +0900

    remove unnecessary method

--- main/model_memoize.py
+++ main/model_memoize.py
@@ -10,15 +10,6 @@ memo = Memoizer(store)
 host = 'mysql+pymysql://root:rootpasswd@localhost/pickle_test?charset=utf8'
 engine = sa.create_engine(host, echo=True)
 
-def load_model(cls, model_path):
-    with open(model_path, 'rb') as model_file:
-        xgb = pickle.load(model_file)
-        return xgb
-
-def find_model_binary():
-    result = engine.execute("SELECT `model` FROM `model_table` WHERE `model_name` = 'xgboost'")
-    model_binary = result.fetchone()
-    return model_binary['model']
 
 def load_test_data():
     test_data = pd.read_csv('../data/test_data.csv')
